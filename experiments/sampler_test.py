59b839196575103414fe60e2b20cf9e831be8929
Daniel Hewitt <dan.hewitt.15@ucl.ac.uk>
2025-04-03 11:43:27 +0100

    Adding Sampler utility to sample with and without replacement

---
+++ experiments/sampler_test.py
@@ -0,0 +1,17 @@
+from src.data_utils.sampling import Sampler
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sampler = Sampler(data)
+
+num_samples = 3
+sampled_with_replacement = sampler.sample_with_replacement(num_samples)
+sampled_without_replacement = sampler.sample_without_replacement(num_samples)
+print("Sampled with replacement:", sampled_with_replacement)
+print("Sampled without replacement:", sampled_without_replacement)
+
+
+num_samples = sampler.calculate_sample_size(0.5)
+sampled_with_replacement = sampler.sample_with_replacement(num_samples)
+sampled_without_replacement = sampler.sample_without_replacement(num_samples)
+print("Sampled with replacement:", sampled_with_replacement)
+print("Sampled without replacement:", sampled_without_replacement)
